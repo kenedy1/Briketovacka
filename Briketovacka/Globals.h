7b320c522118a6b47a66f31122f95d62e5f120d2
Marian <kenedy1@post.sk>
2022-09-21 09:00:53 +0200

    Upravene texty LCD

--- Briketovacka/Globals.h
+++ Briketovacka/Globals.h
@@ -1,8 +1,6 @@
 #ifndef  _Globals_h_
 #define _Globals_h_
 
-
-
 #include <StopwatchLib.h>
 #include <OneWire.h>
 #include <DS18B20.h>
@@ -11,7 +9,6 @@
 #include <RTClib.h>
 #include "IO_Pins.h"
 #include <SerialCommands.h>
-
 
 typedef enum {
 	BROFF,
@@ -26,16 +23,16 @@ typedef enum {
 T_BrikState;
 //Text for disply
 char state_text[][20] = {
-	"BR OFF",
-	"BR ON",
-	"BR ON GO",
-	"ALB TEPLOTA OL",
-	"ALB MALO PILIN",
-	"ALB VRECO PLNE",
-	"ALB FILT OLEJA",
-	"ALB MALO OLEJA"
+	"Briket OFF",
+	"Briket ON",
+	"Briket PRACUJE",
+	"ERR Bri TEPLOT OLEJ",
+	"ERR Bri MALO PILIN",
+	"ERR Bri VRECO PLNE",
+	"ERR Bri FILTER OLEJ",
+	"ERR Bri MALO OLEJA"
+//   12345678901234567890
 };
-
 
 typedef enum {
 	SI_OFF,
@@ -46,21 +43,18 @@ typedef enum {
 }
 T_SiloState;
 char Sil_state_text[][20] = {
-	"SI_OFF",
-	"SI_ON",
-	"PREFUK",
-	"ALSI MALO PIL",
-	"ALSI VELA PIL"
+	"Silo OFF",
+	"Silo ON",
+	"Silo PREFUK",
+	"ERR Silo MALO PILIN",
+	"ERR Silo VELA PILIN"
+	//   12345678901234567890
 };
-
-
-
 
 DS18B20 oilTemp(oneWirePin);
 DateTime myDateTime;
 RTC_DS1307 rtc;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-
 
 bool oilTempSenOK = 0;
 int lastBrikState = 255;
